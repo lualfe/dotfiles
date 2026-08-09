138e16046d2d5742f2916959a4dfcb19bbec1a75
lucas_fe <lucas.albuquerque.felipini@gmail.com>
2026-08-09 10:27:43 -0300

    neovim buffer reload and swap files handling

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -13,3 +13,26 @@ vim.api.nvim_create_autocmd("FileType", {
 vim.opt.relativenumber = true
 vim.opt.cursorline = true
 
+vim.opt.autoread = true
+
+vim.api.nvim_create_autocmd({"FocusGained", "BufEnter"}, {
+  pattern = "*",
+  command = "checktime",
+})
+
+local function ensure_dir(path)
+  vim.fn.mkdir(path, "p")
+end
+
+local swap_dir = vim.fn.expand("~/.local/share/nvim/swap//")
+local backup_dir = vim.fn.expand("~/.local/share/nvim/backup//")
+local undo_dir = vim.fn.expand("~/.local/share/nvim/undo//")
+
+ensure_dir(swap_dir)
+ensure_dir(backup_dir)
+ensure_dir(undo_dir)
+
+vim.opt.directory = swap_dir
+vim.opt.backupdir = backup_dir
+vim.opt.undodir = undo_dir
+vim.opt.undofile = true
